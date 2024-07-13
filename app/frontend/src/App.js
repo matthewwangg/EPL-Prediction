319cffe00fe6d552a8170372645f2e747237421c
Matt Wang <matthewianwang@gmail.com>
2024-07-13 16:31:52 -0400

    Updated navigation bar

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import PredictPage from './pages/PredictPage';
--- app/frontend/src/components/Navbar.js
+++ app/frontend/src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/styles.css';
+import '../styles/navbar.css';
 
 function Navbar() {
     return (
         <header className="header-bar">
+            <div className="title">Player Performance Prediction 2.0</div>
             <nav>
-                <ul>
+                <ul className="nav-links">
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/predict">Predict</Link></li>
                     <li><Link to="/predict-custom">Predict Custom</Link></li>
@@ -15,14 +16,10 @@ function Navbar() {
                 </ul>
             </nav>
             <div className="social-icons">
-                <a href="https://www.instagram.com/matthewiwang/" target="_blank" rel="noopener noreferrer"><i
-                    className="fab fa-instagram"></i></a>
-                <a href="https://github.com/matthewwangg" target="_blank" rel="noopener noreferrer"><i
-                    className="fab fa-github"></i></a>
-                <a href="https://twitter.com/wangganggg" target="_blank" rel="noopener noreferrer"><i
-                    className="fab fa-twitter"></i></a>
-                <a href="https://www.linkedin.com/in/matthew-wang-cs/" target="_blank" rel="noopener noreferrer"><i
-                    className="fab fa-linkedin"></i></a>
+                <a href="https://www.instagram.com/matthewiwang/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram"></i></a>
+                <a href="https://github.com/matthewwangg" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i></a>
+                <a href="https://twitter.com/wangganggg" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+                <a href="https://www.linkedin.com/in/matthew-wang-cs/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
             </div>
         </header>
     );
