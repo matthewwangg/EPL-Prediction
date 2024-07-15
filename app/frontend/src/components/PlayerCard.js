17355ad005706c29c4aa76f6583250ec250f0831
Matt Wang <matthewianwang@gmail.com>
2024-07-14 20:00:51 -0400

    Updated player card styles
    Adjusting the player card to not use an image

--- app/frontend/src/components/PlayerCard.js
+++ app/frontend/src/components/PlayerCard.js
@@ -3,7 +3,7 @@ import '../styles/playercard.css';
 
 const PlayerCard = ({ imgSrc, playerName }) => (
     <div className="photo-container">
-        <img src={imgSrc} alt={playerName} />
+        {/*<img src={imgSrc} alt={playerName} />*/}
         <div className="card-description">
             <p>{playerName}</p>
         </div>
