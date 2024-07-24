0b3b79152128c79e5095c9832887d4efdc32cc37
Matt Wang <matthewianwang@gmail.com>
2024-07-23 20:46:50 -0400

    Updated component and added information

--- app/frontend/src/components/AboutContainer.js
+++ app/frontend/src/components/AboutContainer.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import '../styles/about.css';
 
-const AboutContainer = ({ reverseOrder = false, source }) => {
+const AboutContainer = ({ reverseOrder = false, source, title, description }) => {
     return (
         <div id="target" className={`about-container2 ${reverseOrder ? 'reversed' : ''}`}>
             <div className="about-picture-container">
                 <img className="profile-image" src={source} alt="Profile Picture"/>
             </div>
             <div className="about-text-container2">
-                <h1 className="about-header">Overview</h1>
+                <h1 className="about-header">{title}</h1>
                 <p className="about-description">
-                    I am an incoming Computer Science senior at Arizona State University, looking
-                    to further my experience in the industry. My main interests are Software Engineering and and
-                    Artificial Intelligence, specifically related to Machine Learning. I'm a hardworking individual
-                    who is adaptable to many situations and puts in a daily commitment to learning and
-                    improvement.
+                    {description}
                 </p>
             </div>
         </div>
--- app/frontend/src/pages/AboutPage.js
+++ app/frontend/src/pages/AboutPage.js
@@ -5,6 +5,9 @@ import AboutContainer from '../components/AboutContainer';
 import '../styles/about.css';
 
 const AboutPage = () => {
+
+    const desc = "I am an incoming Computer Science senior at Arizona State University, looking to further my experience in the industry. My main interests are Software Engineering and Artificial Intelligence, specifically related to Machine Learning. I'm a hardworking individual who is adaptable to many situations and puts in a daily commitment to learning and improvement.";
+    const education = "My Computer Science education has spanned the last 3 school years, including classes such as Data Structures and Algorithms, Distributed Software Development, Foundations of Machine Learning, and Operating Systems. I have also been a Undergraduate Teaching Assistant for both Intro to Theoretical Computer Science and Principles of Programming Languages.";
     return (
         <div>
             <TextContainer
@@ -13,7 +16,9 @@ const AboutPage = () => {
                 buttonText="Read More"
                 targetId="target"
             />
-            <AboutContainer reverseOrder={false} source={"/assets/mattw.jpg"} />
+            <AboutContainer reverseOrder={false} source={"/assets/mattw.jpg"} title={"Overview"} description={desc}/>
+            <AboutContainer reverseOrder={true} source={"/assets/mattw.jpg"} title={"Education"} description={education}/>
+
             <FooterContainer />
         </div>
     );
